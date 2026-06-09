abe2f49ab46b1d9d8c46c456ef37281b7ce50161
rohitJung <rokshh28@gmail.com>
2026-06-09 21:11:20 +0545

    feat: add all keys random eviction

--- config/config.go
+++ config/config.go
@@ -1,8 +1,12 @@
 package config
 
 var (
-	Host        string = "127.0.0.1"
-	Port        int    = 7379
-	KeysLimit   int    = 5
+	Host      string = "127.0.0.1"
+	Port      int    = 7379
+	KeysLimit int    = 5
+
 	AofFileName string = "aof-dump.aof"
+
+	EvictionStrategy string  = "all-keys-random"
+	EvictionRatio   float64 = 0.40
 )
--- core/eviction.go
+++ core/eviction.go
@@ -1,5 +1,7 @@
 package core
 
+import "github.com/rohit-Jung/func-redis/config"
+
 func evictFirst() {
 	// the iteration is random on hashmaps
 	for k := range store {
@@ -8,6 +10,23 @@ func evictFirst() {
 	}
 }
 
+func evictAllKeysRandom() {
+	evictCount := int(config.EvictionRatio * float64(config.KeysLimit))
+
+	for key, _ := range store {
+		Delete(key)
+		evictCount--
+		if evictCount == 0 {
+			break
+		}
+	}
+}
+
 func evict() {
-	evictFirst()
+	switch config.EvictionStrategy {
+	case "all-keys-random":
+		evictAllKeysRandom()
+	case "simple-first":
+		evictFirst()
+	}
 }
