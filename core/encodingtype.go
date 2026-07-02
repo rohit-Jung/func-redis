d2cb5d9cc4a7bed55d8bf5d0d9d9fb44a744c56c
rohitJung <rokshh28@gmail.com>
2026-07-02 20:49:11 +0545

    refactor: err namings

--- core/encodingtype.go
+++ core/encodingtype.go
@@ -12,12 +12,12 @@ func getEncoding(val uint8) uint8 {
 	return (val << 4) >> 4
 }
 
-var wrongEncoding = fmt.Errorf("the operation is not permitted on this encoding")
-var wrongType = fmt.Errorf("the operation is not permitted on this type")
+var errWrongEncoding = fmt.Errorf("the operation is not permitted on this encoding")
+var errWrongType = fmt.Errorf("the operation is not permitted on this type")
 
 func assertEncoding(val uint8, actual uint8) error {
 	if getEncoding(val) != actual {
-		return wrongEncoding
+		return errWrongEncoding
 	}
 
 	return nil
@@ -25,7 +25,7 @@ func assertEncoding(val uint8, actual uint8) error {
 
 func assertType(val uint8, actual uint8) error {
 	if getType(val) != actual {
-		return wrongType
+		return errWrongType
 	}
 
 	return nil
